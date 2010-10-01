0c36753464f29e94422b8e652b0be5e362e4e5a5
William Sargent <wsargent@wsargent-mbp-2.local>
2010-10-01 15:51:10 -0700

    Make changes to upgrade to Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+Radiator::Application.routes do  
+  resources :betabrite, :co llection => { :radiate => :get }
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-  map.resources :betabrite, :collection => { :radiate => :get }
-  
-  map.connect 'continuous_integration/radiate/:project/:status', :controller => 'continuous_integration', :action => 'radiate'
-  map.connect 'continuous_integration/dim/:project', :controller => 'continuous_integration', :action => 'dim'
-  
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => :betabrite
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # map.connect ':controller/:action/:id'
-  # map.connect ':controller/:action/:id.:format'
+  root :controller => :betabrite   
 end
