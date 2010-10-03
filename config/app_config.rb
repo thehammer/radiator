793a94e023ba3a42dc5504c4d91f0ae17b90b55a
William Sargent <wsargent@wsargent-mbp-2.local>
2010-10-03 16:46:42 -0700

    Update the app config, change the update display resolution from 5 seconds to 10 seconds.

--- config/app_config.rb
+++ config/app_config.rb
@@ -7,4 +7,5 @@ config_file = File.expand_path('../config.yml', __FILE__)
 config = YAML.load_file(config_file) || {}
 app_config = config['common'] || {}
 app_config.update(config[Rails.env] || {})
+app_config[:message_sources] = []
 AppConfig = OpenStruct.new(app_config)
